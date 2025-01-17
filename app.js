4ffb49fd5c545dfe0128785cd91b323e139c5d24
Asr <julioserrepe1@gmail.com>
2025-01-17 00:44:50 -0600

    Imprime lista de amigos

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 let amigos = []; // Declarando array para almacenar amigos
 
 function agregarAmigo(){
-    let amigo = document.getElementById('amigo').value; // Obtiene el nombre dado por el usuario
+    let amigo = document.getElementById('amigo'); // Obtiene el nombre dado por el usuario
+    let nombreAmigo = amigo.value; // Extrae el nombre
 
-    if(amigo == ''){ // Compara para verificar que el nombre no sea vacio
+    if(nombreAmigo == ''){ // Compara para verificar que el nombre no sea vacio
         alert("Por favor, inserte un nombre."); // Mensaje de alerta al usuario
     } else{
-        amigos.push(amigo); // Si es valido, inserta en el array
+        amigos.push(nombreAmigo); // Si es valido, inserta en el array
+        leerAmigos(amigos); // Añadiendo a los amigos
     }
 
-    amigo = ''; // Borra lo ingresado por el usuario
+    amigo.value = ''; // Borra lo ingresado por el usuario
+}
+
+function leerAmigos(lista){
+    let listaAmigos = document.getElementById('listaAmigos'); // Toma el elemento de 
+    let textoLista = ''; // Inicia el texto con todos los elementos
+    listaAmigos.innerHTML = ''; // Elimina todo el texto de la etiqueta
+
+    for(let i=0; i<lista.length; i++){
+        textoLista += `<li> ${lista[i]} <li/> `; // Agrega elemento de la lista
+    }
+
+    listaAmigos.innerHTML = textoLista; // Agrega el texto como elementos de la lista
 }
