6aa2176c7315117ed54dc7a40a2a8fab8b01ed81
Asr <julioserrepe1@gmail.com>
2025-01-17 00:21:08 -0600

    Implementa en una funcion

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 let amigos = []; // Declarando array para almacenar amigos
 
-let amigo = document.getElementById('amigo').value; // Obtiene el nombre dado por el usuario
+function agregarAmigo(){
+    let amigo = document.getElementById('amigo').value; // Obtiene el nombre dado por el usuario
 
-if(amigo == ''){ // Compara para verificar que el nombre no sea vacio
-    alert("Por favor, inserte un nombre."); // Mensaje de alerta al usuario
-} else{
-    amigos.push(amigo); // Si es valido, inserta en el array
+    if(amigo == ''){ // Compara para verificar que el nombre no sea vacio
+        alert("Por favor, inserte un nombre."); // Mensaje de alerta al usuario
+    } else{
+        amigos.push(amigo); // Si es valido, inserta en el array
+    }
+
+    amigo = ''; // Borra lo ingresado por el usuario
 }
-
-amigo = ''; // Borra lo ingresado por el usuario
