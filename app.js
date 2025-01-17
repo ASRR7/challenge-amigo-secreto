6295db2ed0cbf44bedd41fc36499df668bf41d0d
Asr <julioserrepe1@gmail.com>
2025-01-17 00:18:26 -0600

    Obtiene valor ingresado, valida y borra

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 let amigos = []; // Declarando array para almacenar amigos
+
+let amigo = document.getElementById('amigo').value; // Obtiene el nombre dado por el usuario
+
+if(amigo == ''){ // Compara para verificar que el nombre no sea vacio
+    alert("Por favor, inserte un nombre."); // Mensaje de alerta al usuario
+} else{
+    amigos.push(amigo); // Si es valido, inserta en el array
+}
+
+amigo = ''; // Borra lo ingresado por el usuario
